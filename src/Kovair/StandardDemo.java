97439ae49d04074a9173149fd5b87dd0f36f6035
mn260 <mn260@demo1.sr.kovair.com>
2017-10-09 21:30:00 -0700

    Commited

---
+++ src/Kovair/StandardDemo.java
@@ -0,0 +1,14 @@
+package Kovair;
+
+public class StandardDemo {
+
+	public StandardDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
