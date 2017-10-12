5268e85e218ffdacfb5a63d7a302ca0e2611709c
mn260 <mn260@demo1.sr.kovair.com>
2017-10-12 02:15:38 -0700

    31

--- src/Kovair/StandardDemo2.java
+++ src/Kovair/StandardDemo2.java
@@ -3,7 +3,7 @@ package Kovair;
 public class StandardDemo2 {
 
 	public StandardDemo2() {
-		System.out.print("Mayukh Naaandy");
+		System.out.print("Test Flow on 10/12/2017 after lunchtime");
 	}
 
 }
