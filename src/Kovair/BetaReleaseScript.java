b01584ce2f55ceb1c7a9103db1ec544518949ad0
rs267 <rs267@demo1.sr.kovair.com>
2017-10-18 03:38:19 -0700

    48

---
+++ src/Kovair/BetaReleaseScript.java
@@ -0,0 +1,14 @@
+package Kovair;
+
+public class BetaReleaseScript {
+
+	public BetaReleaseScript() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	System.out.println("Beta Script For Demo Run on 18/10/2017");
+
+	}
+
+}
