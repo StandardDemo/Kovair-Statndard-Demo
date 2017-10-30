4dd3df397f7e178a618db1cb425bbbe120f07004
mn260 <mn260@demo1.sr.kovair.com>
2017-10-29 23:07:58 -0700

    152386656

--- src/Kovair/AlphaScript.java
+++ src/Kovair/AlphaScript.java
@@ -3,11 +3,11 @@ package Kovair;
 public class AlphaScript {
 
 	public AlphaScript() {
-		// TODO Comment added during Demo1
+		// TODO Comment added during Demo1.1
 	}
 
 	public static void main(String[] args) {
-		System.out.println("23/10/2017");
+		System.out.println("User Story ID: 152386656");
 
 	}
 
