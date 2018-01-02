5975426c24c804dd99000ad7d30164477059d3e0
mn260 <mn260@demo1.sr.kovair.com>
2018-01-01 21:40:08 -0800

    153984318

--- src/Kovair/AlphaScript.java
+++ src/Kovair/AlphaScript.java
@@ -7,7 +7,7 @@ public class AlphaScript {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("User Story ID: 153984318");
+		System.out.println("User Story ID: 153984319");
    
 	}
 
