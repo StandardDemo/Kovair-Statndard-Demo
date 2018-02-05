f25521a902c003c8631c3941bf4c0ef891218d85
mn260 <mn260@demo1.sr.kovair.com>
2018-02-05 05:57:17 -0800

    tas

--- Standard Demo/src/com/model1/listfixa.java
+++ Standard Demo/src/com/model1/listfixa.java
@@ -3,7 +3,7 @@ package com.model1;
 public class listfixa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/// TODO Auto-generated method stub
 System.out.println("List Fixed on 05/02/2018 5.25 AM/mPM");
 	}
 
