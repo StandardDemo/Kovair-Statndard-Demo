ee29d829f142e8a6d906173829afb5fe94ec4769
mn260 <mn260@DevOpsApp.sr.kovair.com>
2018-10-05 01:02:11 -0700

    qq

---
+++ CBA Workspace/.sonarlint/modules/Kovair-Statndard-Demo/issues/index.pb
@@ -0,0 +1,3 @@
+
+4
+test,a\9\a94a8fe5ccb19ba61c4c0873d391e987982fbbd3
---
+++ JavaDemoCBA/src/com/javademocba/proj/CBAmodule.java
@@ -0,0 +1,10 @@
+package com.javademocba.proj;
+
+public class CBAmodule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
