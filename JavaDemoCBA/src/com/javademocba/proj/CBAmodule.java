3c144eeedc6122d2282cedd48ca5f89881b45de0
mn260 <mn260@DevOpsApp.sr.kovair.com>
2018-10-08 02:54:52 -0700

    KDP-120

--- JavaDemoCBA/src/com/javademocba/proj/CBAmodule.java
+++ JavaDemoCBA/src/com/javademocba/proj/CBAmodule.java
@@ -1,10 +1,29 @@
 package com.javademocba.proj;
 
-public class CBAmodule {
-
-	public static void main(String[] args) {
-		System.out.println("Hello");
-
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class CBAmodule{
+	public static void main (String srgs[])
+	{
+		Solution a = new Solution();
+		List<String> qa= a.generateParenthesis(5);
+		System.out.println(qa);
 	}
-
 }
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right );
+        }
+        return ans;
+    }
+}
+//This is updated on 08102018
