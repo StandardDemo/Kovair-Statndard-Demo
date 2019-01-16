fcd4b9b24584a4c18015557b2cb06613f3b1c1b1
mn260 <mn260@demo1.sr.kovair.com>
2019-01-16 06:05:20 -0800

    862

--- DemoBetaProject/src/com/betaproject/myjavaapp/UserListPage.java
+++ DemoBetaProject/src/com/betaproject/myjavaapp/UserListPage.java
@@ -3,7 +3,7 @@ package com.betaproject.myjavaapp;
 public class UserListPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubfdsrt
+		// TODO Auto-generated method v1
 
 	}
 
