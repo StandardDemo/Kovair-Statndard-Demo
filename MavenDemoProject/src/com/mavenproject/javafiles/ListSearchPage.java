138422cb528eb6611b8affff39ddeda954b2f07e
rs267 <rs267@demo1.sr.kovair.com>
2019-01-16 20:33:02 -0800

    864

--- MavenDemoProject/src/com/mavenproject/javafiles/ListSearchPage.java
+++ MavenDemoProject/src/com/mavenproject/javafiles/ListSearchPage.java
@@ -1,10 +1,30 @@
 package com.mavenproject.javafiles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListSearchPage {
 
-	public static void main(String[] args) {
+	public static List<String> main(String[] args) {
 		// TODO Auto-generated method stub
+		// Commit for 864
+		ArrayList<String> ans = new ArrayList();
+        int n = 0;
+		if (n == 0) {
+            ans.add("");
+        } else {
+            String left = null;
+			String right = null;
+			for (int c = 0; c < n; ++c)
+         
+                        ans.add("(" + left + ")" + right );
+        }
+        return ans;// modification
+	}
 
+	private static Object generateParenthesis(int c) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
