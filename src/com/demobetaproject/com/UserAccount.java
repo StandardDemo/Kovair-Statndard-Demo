c1d3fe02785f3e45e17c1ce039db1c3e825d7d8b
rs267 <rs267@demo1.sr.kovair.com>
2019-02-06 05:54:39 -0800

    314

---
+++ src/com/demobetaproject/com/UserAccount.java
@@ -0,0 +1,22 @@
+package com.demobetaproject.com;
+
+import java.awt.List;
+
+public class UserAccount {
+	public static final String GENDER_MALE = "M";
+	   public static final String GENDER_FEMALE = "F";
+	 //Checkin for 314
+	   private String userName;
+	   private String gender;
+	   private String password;
+	 
+	   private List roles;
+	 
+	   public UserAccount() {
+	   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
