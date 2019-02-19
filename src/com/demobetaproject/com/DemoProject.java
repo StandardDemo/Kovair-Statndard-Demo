a37791ae63e84f4147aef4a041f34939dc5aecbb
rs267 <rs267@demo1.sr.kovair.com>
2019-02-19 00:18:45 -0800

    KDP-198

--- src/com/demobetaproject/com/DemoProject.java
+++ src/com/demobetaproject/com/DemoProject.java
@@ -5,7 +5,7 @@ import java.awt.List;
 public class DemoProject {
 	public static final String GENDER_MALE = "M";
 	   public static final String GENDER_FEMALE = "Y";
-	 //Checkin for KDP-198
+	 //Checkin for Jira ID KDP-198
 	   private String userName;
 	   private String gender;
 	   private String password;
