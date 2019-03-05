67f7906b80f02b702b10a1f94464586f5acfca15
sk203 <sk203@demo1.sr.kovair.com>
2019-03-05 05:12:05 -0800

    test 2

--- src/com/demobetaproject/com/UserAccount.java
+++ src/com/demobetaproject/com/UserAccount.java
@@ -5,7 +5,7 @@ import java.awt.List;
 public class UserAccount {
 	public static final String GENDER_MALE = "M";
 	   public static final String GENDER_FEMALE = "Y";
-	 //Checkin 1
+	 //Checkin 11
 	   private String userName;
 	   private String gender;
 	   private String password;
